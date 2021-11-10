3c038bb0bcef4fd21ae885325f937204161a4a9b
Yurii Onats <yura.onats@gmail.com>
2021-11-10 11:20:13 +0200

    fix2

--- src/js/markup.js
+++ src/js/markup.js
@@ -18,31 +18,9 @@ function createTemplate(arrey) {
         hideLoader();
         throw new Error('Eroor fetching data');
     }
-}
+};
+
 resultTemplate.addEventListener('click', openModal);
-
-
-//===================
-
-// function lazyLoad(targets) {
-//     const options = {
-//         rootMargin: '100px',
-//     };
-//     const onEntry = (entries, observer) => {
-//         entries.forEach(entry => {
-//             if (entry.isIntersecting) {
-//                 const img = entry.target;
-//                 const src = img.dataset.lazy;
-//                 img.src = src;
-//                 observer.unobserve(img);
-//             };
-//         });
-//     };
-
-//     const io = new IntersectionObserver(onEntry, options);
-
-//     targets.forEach(target => io.observe(target));
-// }
 
 export default createTemplate;
 export { resultTemplate };
