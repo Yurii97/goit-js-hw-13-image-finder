35a686980ace826ac3a460c07b1485825a4a89c4
Yurii Onats <yura.onats@gmail.com>
2021-11-10 09:34:57 +0200

    finich2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const _ = require('lodash');
 
 hideLoader();
 
-// слухач
 const inputRef = document.querySelector('#search-form')
 inputRef.addEventListener('input',
     _.debounce(onSearch, 1000));
