5ea0baff9d6f8202d76d58974b44ac5d3c7e4272
Yurii Onats <yura.onats@gmail.com>
2021-11-10 09:12:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 
 hideLoader();
 
+// слухач
 const inputRef = document.querySelector('#search-form')
 inputRef.addEventListener('input',
     _.debounce(onSearch, 1000));
